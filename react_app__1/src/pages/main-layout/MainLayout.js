5c1e852a247f33820bc91265342d0b28c612c3bd
Murad Masimli <murad.mesimli.99@gmail.com>
2023-08-03 19:24:15 +0400

    feat:products were added

---
+++ react_app__1/src/pages/main-layout/MainLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./MainLayout.module.scss"
+const MainLayout = () => {
+  return (
+    <>
+
+
+        
+    
+    
+    </>
+  )
+}
+
+export default MainLayout
